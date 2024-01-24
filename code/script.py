6c517378a8b7170c89483fe4274f243f25df2b72
Younes Ahrioui <younes.ahrioui@etu.univ-lyon1.fr>
2024-01-24 18:48:47 +0100

    Adressage interconnexion OK

--- code/script.py
+++ code/script.py
@@ -1,43 +1,43 @@
 import json
 import sys
 import time
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 
 base_config = [
-"version 15.2",
-"service timestamps debug datetime msec",
-"service timestamps log datetime msec",
-"boot-start-marker",
-"boot-end-marker",
-"no aaa new-model",
-"no ip icmp rate-limit unreachable",
-"ip cef",
-"no ip domain lookup",
-"ipv6 unicast-routing",
-"ipv6 cef",
-"multilink bundle-name authenticated",
-"ip tcp synwait-time 5",
-"ip forward-protocol nd",
-"no ip http server",
-"no ip http secure-server",
-"control-plane",
-"line con 0",
-" exec-timeout 0 0",
-" privilege level 15",
-" logging synchronous",
-" stopbits 1",
-"line aux 0",
-" exec-timeout 0 0",
-" privilege level 15",
-" logging synchronous",
-" stopbits 1",
-"line vty 0 4",
-" login",
-"end"]
+    "version 15.2",
+    "service timestamps debug datetime msec",
+    "service timestamps log datetime msec",
+    "boot-start-marker",
+    "boot-end-marker",
+    "no aaa new-model",
+    "no ip icmp rate-limit unreachable",
+    "ip cef",
+    "no ip domain lookup",
+    "ipv6 unicast-routing",
+    "ipv6 cef",
+    "multilink bundle-name authenticated",
+    "ip tcp synwait-time 5",
+    "ip forward-protocol nd",
+    "no ip http server",
+    "no ip http secure-server",
+    "control-plane",
+    "line con 0",
+    " exec-timeout 0 0",
+    " privilege level 15",
+    " logging synchronous",
+    " stopbits 1",
+    "line aux 0",
+    " exec-timeout 0 0",
+    " privilege level 15",
+    " logging synchronous",
+    " stopbits 1",
+    "line vty 0 4",
+    " login",
+    "end"]
 
 
 def read_json(file_path):
-    #Essaye de lire et extraire la topologie d'un json, sinon quitte le programme avec un message d'erreur
+    # Essaye de lire et extraire la topologie d'un json, sinon quitte le programme avec un message d'erreur
     try:
         with open(file_path, 'r') as file:
             topology = json.load(file)
@@ -55,21 +55,23 @@ def read_json(file_path):
 def main(topology):
     with ThreadPoolExecutor() as executor:
         futures = []
-
+        subnet_dict = give_subnet_number(topology)
+        subnet_interconnexion_dict = give_subnet_interconnexion(topology, subnet_dict)
         for AS in topology:
-            subnet_dict = give_subnet_number(topology[AS]["routers"])
             for router in topology[AS]['routers']:
-                future = executor.submit(process_router, router, AS, subnet_dict, topology)
+                future = executor.submit(process_router, router, AS, subnet_dict[AS], subnet_interconnexion_dict[AS], topology)
                 futures.append(future)
 
         for future in futures:
             future.result()
 
-def process_router(router, AS, subnet_dict, topology):
+
+def process_router(router, AS, subnet_dict, subnet_interconnexion_dict, topology):
     create_base_cfg(router, base_config)
     create_loopback_interface(router, topology[AS])
-    create_router_interfaces(router, topology[AS], subnet_dict)
-    activate_protocols(router, AS, topology[AS])
+    create_router_interfaces(router, AS, topology[AS], subnet_dict)
+    activate_protocols(router, AS, topology[AS], subnet_interconnexion_dict)
+
 
 def insert_cfg_line(router, index_line, data):
     # insert une information 'data' à la ligne 'line'.
@@ -99,12 +101,11 @@ def find_index(router, line):
     return index_line
 
 
-def create_router_interfaces(router, as_topology, subnet_dict):
+def create_router_interfaces(router, AS, as_topology, subnet_dict):
     # insert dans le cfg du routeur toutes les ses interfaces et leurs ipv6 correspondantes.
     index_line = find_index(router, line="ip tcp synwait-time 5\n")
 
     for neighbor in as_topology['routers'][router].keys():
-
         if router[1:] < neighbor[1:]:
             subnet_index = subnet_dict[(router, neighbor)]
             router_index = 1
@@ -112,42 +113,71 @@ def create_router_interfaces(router, as_topology, subnet_dict):
             subnet_index = subnet_dict[(neighbor, router)]
             router_index = 2
 
-        insert_cfg_line(router, index_line, f"interface {as_topology['routers'][router][neighbor]}\n no ip address\n negotiation auto\n ipv6 address {as_topology['address']}{subnet_index}::{router_index}{as_topology['subnet_mask']}\n ipv6 enable\n")
+        insert_cfg_line(router, index_line,
+                        f"interface {as_topology['routers'][router][neighbor]}\n no ip address\n negotiation auto\n ipv6 address {as_topology['address']}{subnet_index}::{router_index}{as_topology['subnet_mask']}\n ipv6 enable\n")
         index_line += 5
 
 
-def give_subnet_number(as_topology):
-    # Creer un dictionnaire qui pour chaque arrête d'une AS, donne un numero de subnet : {(R1,R2):1, (R2,R3):2...}
-    subnet_number = 1
+def give_subnet_number(as_topology):  # Crée le tableau des subnets pour tout le json
     subnet_dict = dict()
-    for router in as_topology:
-        for neighbor in as_topology[router]:
-            if router[1:] < neighbor[1:]:
-                subnet_dict[(router, neighbor)] = subnet_number
-                subnet_number += 1
+    for AS in as_topology:
+        subnet_dict[AS] = dict()
+        subnet_number = 1
+        for router in as_topology[AS]['routers']:
+            for neighbor in as_topology[AS]['routers'][router]:
+                if router[1:] < neighbor[1:]:
+                    subnet_dict[AS][(router, neighbor)] = subnet_number
+                    subnet_number += 1
     return subnet_dict
+
+def last_entries_subnet(subnet_dict):
+    last_entry = dict()
+    for AS in subnet_dict:
+        last_entry[AS] = list(subnet_dict[AS].values())[-1]
+    return last_entry
+def give_subnet_interconnexion(as_topology, subnet_dict):  # Crée le tableau des subnets des interconnexion entre AS pour tout le json
+    subnet_interconnexion_dict = dict()
+    last_entries = last_entries_subnet(subnet_dict)
+    for AS in as_topology:
+        subnet_interconnexion_dict[AS] = dict()
+
+    for AS in as_topology:
+        for AS_neighbor in as_topology[AS]['neighbor']:
+            for router1 in as_topology[AS]['neighbor'][AS_neighbor]:
+                for router2 in as_topology[AS]['neighbor'][AS_neighbor][router1]:
+                    if (router1, router2) not in subnet_interconnexion_dict[AS].keys() and (router2, router1) not in subnet_interconnexion_dict[AS].keys():
+                        if AS < AS_neighbor:
+                            subnet_interconnexion_dict[AS][(router1, router2)] = str(int(AS[3:])) + str((last_entries[AS] + 1)) + "::1"
+                            subnet_interconnexion_dict[AS_neighbor][(router2, router1)] = str(int(AS[3:])) + str((last_entries[AS] + 1)) + "::2"
+                        else:
+                            subnet_interconnexion_dict[AS][(router1, router2)] = str(int(AS_neighbor[3:])) + str((last_entries[AS] + 1)) + "::1"
+                            subnet_interconnexion_dict[AS_neighbor][(router2, router1)] = str(int(AS_neighbor[3:])) + str((last_entries[AS] + 1)) + "::2"
+                        last_entries[AS] += 1
+    return subnet_interconnexion_dict
+
 
 def is_rip(as_topology):
     # retourne True si RIP est à activer, False sinon.
     return True if as_topology["protocol"] == "RIP" else False
+
 
 def is_ospf(as_topology):
     # retourne True si RIP est à activer, False sinon
     return True if as_topology["protocol"] == "OSPF" else False
 
 
-def activate_protocols(router, AS, as_topology):
+def activate_protocols(router, AS, as_topology, subnet_interconnexion_dict):
     # active tous les protocols d'un routeur.
     router_id = give_router_id(router)
     if is_ospf(as_topology):
         activate_ospf(router, as_topology, router_id)
     elif is_rip(as_topology):
         activate_rip(router, as_topology)
-    activate_bgp(router, AS, as_topology)
+    activate_bgp(router, AS, as_topology, subnet_interconnexion_dict)
 
 
 def activate_ospf(router, as_topology, router_id):
-    #active OSPF sur le routeur.
+    # active OSPF sur le routeur.
     index_line = find_index(router, "no ip http secure-server\n")
     insert_cfg_line(router, index_line, f"ipv6 router ospf 1\n router-id {router_id}\n")
     for interface in as_topology["routers"][router].values():
@@ -172,47 +202,49 @@ def activate_rip(router, as_topology):
         index_line = find_index(router, f"interface {interface}\n") + 4
         insert_cfg_line(router, index_line, f" ipv6 rip {rip_process_name} enable\n")
 
+
 def give_router_id(router):
     x = router[1:]
     return f"{x}.{x}.{x}.{x}"
 
-def give_subnet_interconnexion(AS1, AS2, routeur1, routeur2):
-    if (int(AS1[3:]) < int(AS2[3:])):
-        return str(int(AS1[3:])*1000 + 999 - int(AS2[3:]) - ((int(routeur1[1:]) if int(routeur1[1:]) < int(routeur2[1:]) else int(routeur2[1:])))) + "::" + ("1" if int(routeur1[1:]) < int(routeur2[1:]) else "2")
-    else:
-        return str(int(AS2[3:])*1000 + 999 - int(AS1[3:]) - (int(routeur1[1:]) if int(routeur1[1:]) < int(routeur2[1:]) else int(routeur2[1:]))) + "::" + ("1" if int(routeur1[1:]) < int(routeur2[1:]) else "2")
+
+def get_subnet_interconnexion(subnet_interconnexion_dict, routeur1, routeur2):
+    return subnet_interconnexion_dict[(routeur1, routeur2)] or subnet_interconnexion_dict[(routeur2, routeur1)]
 
 
-def activate_bgp(routeur, AS, as_topology):
-        index_line = find_index(routeur, "ip forward-protocol nd\n") - 1
-        if is_border_routers(routeur, as_topology):
-            for AS_neighbor in as_topology["neighbor"]:
-                for neighborRouter in as_topology["neighbor"][AS_neighbor]:
-                    if neighborRouter == routeur:
-                        for neighborRouter2 in as_topology["neighbor"][AS_neighbor][neighborRouter]:
-                            insert_cfg_line(routeur, index_line, f"interface {as_topology["neighbor"][AS_neighbor][neighborRouter][neighborRouter2]}\n no ip address\n negotiation auto\n ipv6 address {as_topology['address'][:-1]}{give_subnet_interconnexion(AS,AS_neighbor,routeur, neighborRouter2)}{as_topology['subnet_mask']}\n ipv6 enable\n")
-                            index_line += 5
-        insert_cfg_line(routeur, index_line, f"router bgp 10{AS[3:]}\n bgp router-id {give_router_id(routeur)}\n bgp log-neighbor-changes\n no bgp default ipv4-unicast\n")
-        index_line += 4
-        neighborConf = ""
-        index_sum = 0
-        for router in as_topology["routers"]:
-            if router != routeur:
-                neighborConf += f" neighbor 2001::{router[1:]} remote-as 10{AS[3:]}\n neighbor 2001::{router[1:]} update-source Loopback0\n"
-                index_sum += 2
-        insert_cfg_line(routeur, index_line, neighborConf)
-        index_line += index_sum
-        insert_cfg_line(routeur, index_line, f" address-family ipv4\n exit-address-family\n address-family ipv6\n")
-        index_line += 3
-        neighborConf = ""
-        index_sum = 0
-        for router in as_topology["routers"]:
-            if router != routeur:
-                neighborConf += f"  neighbor 2001::{router[1:]} activate\n"
-                index_sum += 1
-        insert_cfg_line(routeur, index_line, neighborConf)
-        index_line += index_sum
-        insert_cfg_line(routeur, index_line, " exit-address-family\n")
+def activate_bgp(routeur, AS, as_topology, subnet_interconnexion_dict):
+    index_line = find_index(routeur, "ip forward-protocol nd\n") - 1
+    if is_border_routers(routeur, as_topology):
+        for AS_neighbor in as_topology["neighbor"]:
+            for neighborRouter in as_topology["neighbor"][AS_neighbor]:
+                if neighborRouter == routeur:
+                    for neighborRouter2 in as_topology["neighbor"][AS_neighbor][neighborRouter]:
+                        insert_cfg_line(routeur, index_line,
+                                        f"interface {as_topology["neighbor"][AS_neighbor][neighborRouter][neighborRouter2]}\n no ip address\n negotiation auto\n ipv6 address {as_topology['address'][:-1]}{get_subnet_interconnexion(subnet_interconnexion_dict, routeur, neighborRouter2)}{as_topology['subnet_mask']}\n ipv6 enable\n")
+                        index_line += 5
+    insert_cfg_line(routeur, index_line,
+                    f"router bgp 10{AS[3:]}\n bgp router-id {give_router_id(routeur)}\n bgp log-neighbor-changes\n no bgp default ipv4-unicast\n")
+    index_line += 4
+    neighborConf = ""
+    index_sum = 0
+    for router in as_topology["routers"]:
+        if router != routeur:
+            neighborConf += f" neighbor 2001::{router[1:]} remote-as 10{AS[3:]}\n neighbor 2001::{router[1:]} update-source Loopback0\n"
+            index_sum += 2
+    insert_cfg_line(routeur, index_line, neighborConf)
+    index_line += index_sum
+    insert_cfg_line(routeur, index_line, f" address-family ipv4\n exit-address-family\n address-family ipv6\n")
+    index_line += 3
+    neighborConf = ""
+    index_sum = 0
+    for router in as_topology["routers"]:
+        if router != routeur:
+            neighborConf += f"  neighbor 2001::{router[1:]} activate\n"
+            index_sum += 1
+    insert_cfg_line(routeur, index_line, neighborConf)
+    index_line += index_sum
+    insert_cfg_line(routeur, index_line, " exit-address-family\n")
+
 
 def is_border_routers(router, as_topology):
     state = False
@@ -221,6 +253,7 @@ def is_border_routers(router, as_topology):
             state = True
     return state
 
+
 def create_loopback_interface(router, as_topology):
     index_line = find_index(router, line="ip tcp synwait-time 5\n")
     insert_cfg_line(router, index_line, f"interface Loopback0\n no ip address\n ipv6 address 2001::{router[1:]}/128\n")
@@ -228,9 +261,10 @@ def create_loopback_interface(router, as_topology):
     if is_ospf(as_topology):
         insert_cfg_line(router, index_line, " ipv6 ospf 1 area 0\n")
 
+
 if __name__ == "__main__":
     start = time.time()
     topology = read_json("intents.json")
     main(topology)
     end = time.time()
-    print(end-start)
+    print(end - start)
